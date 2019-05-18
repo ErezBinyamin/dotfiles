fe92555612ef4763a6e21fe8aa1a5f1933ea9e38
My Name <foo@example.com>
2019-05-17 23:55:50 -0400

    Added git comments

--- git.sh
+++ git.sh
@@ -14,26 +14,43 @@ alias gl1='git log -n 1'
 alias gr='git reset HEAD'
 alias grhh='git reset --hard HEAD'
 
-# Prompt
-# COLORS:
+# Remote interaction
+alias gps='git push'
+alias gpl='git pull'
+
+# Git command line prompt
+
+# GIT REPO:
+#       Shows name of current git repo in random color
+#       Shows oposite color on arrows (Incase of unreadable color)
+__git_repo='`[ $PROMPT_GIT_PARSE -eq 1 ] && \
+[[ "$(git rev-parse --git-dir 2> /dev/null)" =~ git ]] && \
+printf "\[\033[00m\] " && \
+printf "\[\033[1;38;5;"\
+"$(( 255 - $(git rev-parse --show-toplevel | xargs basename | md5sum | tr -d -c 0-9 | cut -c 1-18 | sed "s/^0*//") % 255 ))"\
+"m\]◀ " && \
+printf "\[\033[00m\]\[\033[1;4;38;5;"\
+"$(( $(git rev-parse --show-toplevel | xargs basename | md5sum | tr -d -c 0-9 | cut -c 1-18 | sed "s/^0*//") % 255 ))"\
+"m\]$(git rev-parse --show-toplevel | xargs basename)" && \
+printf "\[\033[00m\]\[\033[1;38;5;"\
+"$(( 255 - $(git rev-parse --show-toplevel | xargs basename | md5sum | tr -d -c 0-9 | cut -c 1-18 | sed "s/^0*//") % 255 ))"\
+"m\] ▶" && \
+printf "\[\033[00m\]"`'
+
+# GIT COLOR:
+#       Generates color based upon local change status
 #       Green : Up to date
 #       Yellow: Ready to commit
 #       Red   : Unstaged changes
-__git_repo='`[ $PROMPT_GIT_PARSE -eq 1 ] && \
-[[ "$(git rev-parse --git-dir 2> /dev/null)" =~ git ]] && \
-printf " \[\033[1;38;5;6m\][\[\033[00m\]" && \
-printf "\[\033[1;38;5;"\
-"$(( $(git rev-parse --show-toplevel | xargs basename | md5sum | tr -d -c 0-9 | cut -c 1-18 | sed "s/^0*//") % 255 ))"\
-"m\]◀ $(git rev-parse --show-toplevel | xargs basename) ▶"\
-&& printf "\[\033[1;38;5;6m\]]\[\033[00m\]"`'
-
 __git_color='`[ $PROMPT_GIT_PARSE -eq 1 ] && \
 [[ "$(git rev-parse --git-dir 2> /dev/null)" =~ git ]] && \
-printf " \[\033[1;48;5;2m\]" && \
-printf "$(git diff-index --quiet --cached HEAD -- || printf "\[\033[1;48;5;3m\]")" && \
-printf "$(git diff --quiet || printf "\[\033[1;48;5;1m\]*")" && \
-printf "$( [ -z "$(git ls-files --exclude-standard --others)" ] || printf "\[\033[1;48;5;1m\]+")"`'
+printf " \[\033[1;38;5;2m\]" && \
+printf "$(git diff-index --quiet --cached HEAD -- || printf "\[\033[1;38;5;3m\]")" && \
+printf "$(git diff --quiet || printf "\[\033[1;38;5;1m\]*")" && \
+printf "$( [ -z "$(git ls-files --exclude-standard --others)" ] || printf "\[\033[1;38;5;1m\]+")"`'
 
+# GIT BRANCH:
+#       Prints current git branch
 __git_branch='`[ $PROMPT_GIT_PARSE -eq 1 ] && \
 [[ "$(git rev-parse --git-dir 2> /dev/null)" =~ git ]] && \
 git branch 2> /dev/null | grep -e ^* | sed "s:* ::"`'
