909c73ba0c3dacbcb32f885415b7e8f9a0029dd8
Your Name <you@example.com>
2019-10-07 13:31:59 -0400

    converted multiline nonsense to cleaner if statement

--- prompt.sh
+++ prompt.sh
@@ -15,12 +15,16 @@ RST="\[\033[00m\]"
 
 # Battery life and charging status
 __bat_life='`[ -f /sys/class/power_supply/BAT1/capacity ] && [[ $(cat /sys/class/power_supply/BAT1/status) != "Discharging" ]] && printf "⚡";\
-[ -f /sys/class/power_supply/BAT1/capacity ] && [ $(cat /sys/class/power_supply/BAT1/capacity) -ge 75 -a $(cat /sys/class/power_supply/BAT1/capacity) -lt 101 ] && printf "\[\033[38;5;10m\]🔋$(cat /sys/class/power_supply/BAT1/capacity)%%";\
-[ -f /sys/class/power_supply/BAT1/capacity ] && [ $(cat /sys/class/power_supply/BAT1/capacity) -ge 50 -a $(cat /sys/class/power_supply/BAT1/capacity) -lt 75 ] && printf "\[\033[38;5;11m\]🔋$(cat /sys/class/power_supply/BAT1/capacity)%%";\
-[ -f /sys/class/power_supply/BAT1/capacity ] && [ $(cat /sys/class/power_supply/BAT1/capacity) -ge 25 -a $(cat /sys/class/power_supply/BAT1/capacity) -lt 50 ] && printf "\[\033[38;5;202m\]🔋$(cat /sys/class/power_supply/BAT1/capacity)%%";\
-[ -f /sys/class/power_supply/BAT1/capacity ] && [ $(cat /sys/class/power_supply/BAT1/capacity) -ge 10 -a $(cat /sys/class/power_supply/BAT1/capacity) -lt 25 ] && printf "\[\033[38;5;9m\]🔋$(cat /sys/class/power_supply/BAT1/capacity)%";\
-[ -f /sys/class/power_supply/BAT1/capacity ] && [ $(cat /sys/class/power_supply/BAT1/capacity) -lt 10 ] && printf "\[\033[38;5;9m\]\[\033[5m\]🔋$(cat /sys/class/power_supply/BAT1/capacity)%%";\
-printf "\[\033[0m\]"
+if [ -f /sys/class/power_supply/BAT1/capacity ];\
+then\
+	BAT=$(cat /sys/class/power_supply/BAT1/capacity);\
+	[ $BAT -ge 75 -a $BAT -lt 101 ] && printf "\[\033[38;5;10m\]🔋$(cat /sys/class/power_supply/BAT1/capacity)%%";\
+	[ $BAT -ge 50 -a $BAT -lt 75 ] && printf "\[\033[38;5;11m\]🔋$(cat /sys/class/power_supply/BAT1/capacity)%%";\
+	[ $BAT -ge 25 -a $BAT -lt 50 ] && printf "\[\033[38;5;202m\]🔋$(cat /sys/class/power_supply/BAT1/capacity)%%";\
+	[ $BAT -ge 10 -a $BAT -lt 25 ] && printf "\[\033[38;5;9m\]🔋$(cat /sys/class/power_supply/BAT1/capacity)%";\
+	[ $BAT -lt 10 ] && printf "\[\033[38;5;9m\]\[\033[5m\]🔋$(cat /sys/class/power_supply/BAT1/capacity)%%";\
+fi;\
+printf "\[\033[0m\]"\
 `'
 
 #Date and time
