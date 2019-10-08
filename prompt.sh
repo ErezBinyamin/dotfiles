0ceebdd7025c90b7d35e8b2b87e92cea0f95ac83
Your Name <you@example.com>
2019-10-08 09:36:54 -0400

    Probably should also check for directory :(

--- prompt.sh
+++ prompt.sh
@@ -15,7 +15,7 @@ RST="\[\033[00m\]"
 
 # Battery life colored and charging status
 __bat_life='`
-if ls /sys/class/power_supply/ | grep -q BAT && [ $(echo $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) | wc -c) -gt 3 ]
+if [ -d /sys/class/power_supply/ ] && ls /sys/class/power_supply/ | grep -q BAT && [ $(echo $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) | wc -c) -gt 3 ]
 then
 	[[ $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) != "Discharging" ]] && printf "»"
 	BAT=$(find /sys/class/power_supply/BAT*/ -name capacity -exec cat {} \;)
