a5d32a844948d6e88d7142659876cb3ae5432dec
Your Name <you@example.com>
2019-10-07 21:38:11 -0400

    Fixed stupidity

--- prompt.sh
+++ prompt.sh
@@ -19,13 +19,13 @@ __bat_life='`
 if [ $(echo $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) | wc -c) -gt 3 ]
 then
 	[[ $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) != "Discharging" ]] && printf "⚡"
-	BAT=$(cat $(find /sys/class/power_supply/BAT*/ -name capacity))
+	BAT=$(find /sys/class/power_supply/BAT*/ -name capacity -exec cat {} \;)
 	[ $BAT -ge 75 -a $BAT -lt 101 ] && printf "\[\033[38;5;10m\]"
 	[ $BAT -ge 50 -a $BAT -lt 75 ] && printf "\[\033[38;5;11m\]"
 	[ $BAT -ge 25 -a $BAT -lt 50 ] && printf "\[\033[38;5;202m\]"
 	[ $BAT -ge 10 -a $BAT -lt 25 ] && printf "\[\033[38;5;9m\]"
 	[ $BAT -lt 10 ] && printf "\[\033[38;5;9m\]\[\033[5m\]"
-	printf "🔋$(cat /sys/class/power_supply/BAT1/capacity)%%"
+	printf "🔋${BAT}%%"
 fi
 printf "\[\033[0m\]"
 `'
