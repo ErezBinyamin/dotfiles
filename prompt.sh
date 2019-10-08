927a8797120796730112f0b889f214787cfd2545
Your Name <you@example.com>
2019-10-07 21:44:16 -0400

    Use find instead of hardcoded path

--- prompt.sh
+++ prompt.sh
@@ -15,15 +15,15 @@ RST="\[\033[00m\]"
 
 # Battery life colored and charging status
 __bat_life='`
-if [ -f /sys/class/power_supply/BAT1/capacity ]
+if [ $(echo $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) | wc -c) -gt 3 ]
 then
-	[[ $(cat /sys/class/power_supply/BAT1/status) != "Discharging" ]] && printf "»"
-	BAT=$(cat /sys/class/power_supply/BAT1/capacity)
-	[ $BAT -ge 75 -a $BAT -lt 101 ] && printf "\[\033[38;5;10m\][||$(cat /sys/class/power_supply/BAT1/capacity)||]"
-	[ $BAT -ge 50 -a $BAT -lt 75 ] && printf "\[\033[38;5;11m\][||$(cat /sys/class/power_supply/BAT1/capacity)| ]"
-	[ $BAT -ge 25 -a $BAT -lt 50 ] && printf "\[\033[38;5;202m\][||$(cat /sys/class/power_supply/BAT1/capacity)  ]"
-	[ $BAT -ge 10 -a $BAT -lt 25 ] && printf "\[\033[38;5;9m\][| $(cat /sys/class/power_supply/BAT1/capacity)  ]"
-	[ $BAT -lt 10 ] && printf "\[\033[38;5;9m\]\[\033[5m\][| $(cat /sys/class/power_supply/BAT1/capacity)  ]"
+	[[ $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) != "Discharging" ]] && printf "»"
+	BAT=$(find /sys/class/power_supply/BAT*/ -name capacity -exec cat {} \;)
+	[ $BAT -ge 75 -a $BAT -lt 101 ] && printf "\[\033[38;5;10m\][||${BAT}||]"
+	[ $BAT -ge 50 -a $BAT -lt 75 ] && printf "\[\033[38;5;11m\][||${BAT}| ]"
+	[ $BAT -ge 25 -a $BAT -lt 50 ] && printf "\[\033[38;5;202m\][||${BAT}  ]"
+	[ $BAT -ge 10 -a $BAT -lt 25 ] && printf "\[\033[38;5;9m\][| ${BAT}  ]"
+	[ $BAT -lt 10 ] && printf "\[\033[38;5;9m\]\[\033[5m\][| ${BAT}  ]"
 fi
 printf "\[\033[0m\]"
 `'
