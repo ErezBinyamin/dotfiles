8e690d934ead366bdd7639259ac30a7cad20e2f5
Your Name <you@example.com>
2020-01-03 14:13:47 -0500

    rearanged battery emoji symbols

--- prompt.sh
+++ prompt.sh
@@ -20,14 +20,19 @@ BATTERY_CHARGING="⚡"
 BATTERY_SYMBOL="🔋"
 if [ -d /sys/class/power_supply/ ] && ls /sys/class/power_supply/ | grep -q BAT && [ $(echo $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) | wc -c) -gt 3 ]
 then
-	[[ $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) != "Discharging" ]] && printf "${BATTERY_CHARGING}"
+	if [[ $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) != "Discharging" ]]
+	then
+		printf "${BATTERY_CHARGING}"
+	fi
+	
 	BAT=$(find /sys/class/power_supply/BAT*/ -name capacity -exec cat {} \;)
 	[ $BAT -ge 75 -a $BAT -lt 101 ] && printf "\[\033[38;5;10m\]"
 	[ $BAT -ge 50 -a $BAT -lt 75 ] && printf "\[\033[38;5;11m\]"
 	[ $BAT -ge 25 -a $BAT -lt 50 ] && printf "\[\033[38;5;202m\]"
 	[ $BAT -ge 10 -a $BAT -lt 25 ] && printf "\[\033[38;5;9m\]"
 	[ $BAT -lt 10 ] && printf "\[\033[38;5;9m\]\[\033[5m\]"
-	printf "${BATTERY_SYMBOL}${BAT}%%"
+	printf "${BAT}%%"
+	printf "${BATTERY_SYMBOL}"
 fi
 printf "\[\033[0m\]"
 `'
