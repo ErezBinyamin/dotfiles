b740ec97e249c9e1bde10f8c0a0ef32382c1dd32
Your Name <you@example.com>
2020-03-25 23:53:29 -0400

    Added ghome command to open firefox homepage of gitRepo

--- git.sh
+++ git.sh
@@ -23,6 +23,7 @@ alias groh='git fetch origin; git reset --hard origin/master' # Reset really har
 # Remote interaction
 alias gps='git push'
 alias gpl='git pull'
+alias ghome="git config --get remote.origin.url | sed 's/\.git//; s/git@/www\./; s#https://#www\.#; s#\.com:#\.com/#' | xargs firefox"
 
 # Submodules
 alias gsf='git submodule foreach'
