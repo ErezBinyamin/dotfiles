c92d1de662ccffe239b60710d2d70e53812aa9c2
Your Name <you@example.com>
2020-04-05 06:28:19 -0400

    Added percentage measure for battery

--- prompt/battery.sh
+++ prompt/battery.sh
@@ -6,13 +6,14 @@ BATTERY_CHARGING="⚡"
 BATTERY_DISCHARGING="🔋"
 if [ $PROMPT_BATTERY -eq 1 ] && [ -d /sys/class/power_supply/ ] && ls /sys/class/power_supply/ | grep -q BAT && [ $(echo $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) | wc -c) -gt 3 ]
 then
-	[[ $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) != "Discharging" ]] && printf "${BATTERY_CHARGING}" || printf "${BATTERY_DISCHARGING}"
 	BAT=$(find /sys/class/power_supply/BAT*/ -name capacity -exec cat {} \;)
-	[ $BAT -ge 75 -a $BAT -lt 101 ] && printf "\[\033[38;5;10m\][||${BAT}||]"
-	[ $BAT -ge 50 -a $BAT -lt 75 ] && printf "\[\033[38;5;11m\][||${BAT}| ]"
-	[ $BAT -ge 25 -a $BAT -lt 50 ] && printf "\[\033[38;5;202m\][||${BAT}  ]"
-	[ $BAT -ge 10 -a $BAT -lt 25 ] && printf "\[\033[38;5;9m\][| ${BAT}  ]"
-	[ $BAT -lt 10 ] && printf "\[\033[38;5;9m\]\[\033[5m\][| ${BAT}  ]"
+	[ $BAT -ge 75 -a $BAT -lt 101 ] && printf "\[\033[38;5;10m\]"
+	[ $BAT -ge 50 -a $BAT -lt 75 ] && printf "\[\033[38;5;11m\]"
+	[ $BAT -ge 25 -a $BAT -lt 50 ] && printf "\[\033[38;5;202m\]"
+	[ $BAT -ge 10 -a $BAT -lt 25 ] && printf "\[\033[38;5;9m\]"
+	[ $BAT -lt 10 ] && printf "\[\033[38;5;9m\]\[\033[5m\]"
+	printf "${BAT}%%"
+	[[ $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) != "Discharging" ]] && printf "${BATTERY_CHARGING}" || printf "${BATTERY_DISCHARGING}"
 fi
 printf "\[\033[0m\]"
 `'
