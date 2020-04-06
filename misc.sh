a69a66cae21dc7ce662b5cad8c1d41aa933d1c5d
Your Name <you@example.com>
2020-04-06 17:24:52 -0400

    RFC_get PDF link support, helpfull error message

--- misc.sh
+++ misc.sh
@@ -70,31 +70,44 @@ RFC_get()
 {
 	[ $# -lt 1 ] && return 1
 	RFC=/tmp/rfc.txt
-	BAD_RFCS=("8" "9" "14" "26" "51" "92")	# TODO find all "BAD RFCs"
+	MIN_RFC=1
 	MAX_RFC=8650
 	isNum='^[0-9]+$'
+	NEVER_ISSUED=( "3333" "3350" "3399" "3699" "3799" "3800" "3899" "3900" "3907" "3908" "3999" "4000" "4099" "4100" "4199" "4200" "4232" "4299" "4300" "4399" "4400" "4499" "4500" "4599" "4600" "4658" "4699" "4700" "4751" "4799" "4800" "4899" "4900" "4921" "4922" "4989" "4999" "5099" "5100" "5108" "5199" "5200" "5299" "5300" "5312" "5313" "5314" "5315" "5319" "5399" "5400" "5499" "5500" "5599" "5600" "5699" "5700" "5799" "5800" "5809" "5821" "5822" "5823" "5899" "5900" "5999" "6000" "6099" "6100" "6102" "6103" "6199" "6200" "6299" "6300" "6399" "6400" "6499" "6500" "6523" "6524" "6599" "6600" "6634" "6699" "6700" "6799" "6800" "6899" "6900" "6966" "6995" "6999" "7000" "7099" "7327" "7907" "8523" "8524" "8535" "8566" "8626" "8644" "8646" "8647" "8648" )
 
 	# Get corresponding RFC by number
 	if [[ ${1} =~ $isNum ]]
 	then
-		[ $1 -gt $MAX_RFC ] && return 1
-		curl "https://www.ietf.org/rfc/rfc${1}.txt" 2>/dev/null > ${RFC}
+		# Validate RFC range
+		if [ $1 -gt $MAX_RFC ] || [ $1 -lt $MIN_RFC ]
+		then
+			echo "Valid RFC numbers: [ ${MIN_RFC} - ${MAX_RFC} ]"
+			return 1
+		fi
+		# Retrieve RFC <N>
+		curl "https://www.ietf.org/rfc/rfc${1}.txt" --write-out %{http_code} --silent --output ${RFC} 2>/dev/null | grep -q '200'
+		if [ $? -eq 0 ]
+		then
+			# Format nicely and print
+			sed -i '/Page [0-9]/,+2d; /page [0-9]/,+2d' ${RFC}
+			cat ${RFC} | grep -q '<!DOCTYPE html>' && return 1 || cat -s ${RFC}
+			return 0
+		fi
+		# Attempt to retrieve PDF link to RFC <N>
+		curl "https://www.rfc-editor.org/info/rfc${1}" --write-out %{http_code} --silent --output ${RFC} 2>/dev/null | grep -q '200'
+		[ $? -eq 0 ] && grep -o 'https.*\.pdf' ${RFC} || echo "RFC ${1} not issued"
 	# Print list of available RFCs
 	elif [[ "${1,,}" == ":list" ]]
 	then
 		for i in `seq 1 $MAX_RFC`
 		do
-			echo "${BAD_RFCS[@]}" | tr ' ' '\n' | grep -qFx $i || echo $i
+			echo "${NEVER_ISSUED[@]}" | tr ' ' '\n' | grep -qFx $i || echo $i
 		done
+	# Print list of RFCs related to keyword
 	else
-		# Print list of RFCs related to keyword
 		ARG="${@}"
 		curl "https://www.rfc-editor.org/search/rfc_search_detail.php?title=${ARG}" 2> /dev/null | sed 's/href="/\n/g; s/.html/.html\n/g' | grep -A 1 --color=auto 'http.*.html' | sed '/boldtext/d; s/"target/<"target/g; s/<[^>]*>//g; /HTML,/d; s/HTML//; /--/d' | grep -v -B 1 html | rev | sed 's/lmth\.//; s/cfr.*//; /--/d' | rev | sed 's/[A-Z]\..*//; /mail-archive/,+1d; s/^[ \t]*//' | sed  's/&nbsp.*//g; s/<a//g; N ; s/\n/:\t/' > $RFC
 	fi
-
-	# Format nicely and print
-	sed -i '/Page [0-9]/,+2d; /page [0-9]/,+2d' ${RFC}
-	cat ${RFC} | grep -q '<!DOCTYPE html>' && return 1 || cat -s ${RFC}
 }
 
 # Travel up some number of directories
