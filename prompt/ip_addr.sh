65cfc98b088870f95306a31a4b885af356227a84
Your Name <you@example.com>
2020-04-16 08:57:47 -0400

    changed prompt seperator to :

--- prompt/ip_addr.sh
+++ prompt/ip_addr.sh
@@ -4,28 +4,28 @@
 if [ $PROMPT_SLOW_NETWORK -eq 0 ]
 then
 	export __prompt_ip_addr='`
-	if [ ${PROMPT_IP_ADDR} -eq 1 ]
+	if [ ${PROMPT_IP_ADDR} -eq 1 ] && printf ":"
 	then
 		if whereis nc &>/dev/null && hostname -I &>/dev/null
 		then
-			nc -w 3 -z 8.8.8.8 53 && hostname -I | cut -d" " -f1 || echo OFFLINE
+			nc -w 3 -z 8.8.8.8 53 && hostname -I | cut -d" " -f1 || echo "OFFLINE"
 		else
-			ip addr | grep inet | grep global | head -n1 | tr " " "\t" | tr "//" "\t"| cut -f6 || echo OFFLINE
+			ip addr | grep inet | grep global | head -n1 | tr " " "\t" | tr "//" "\t"| cut -f6 || echo "OFFLINE"
 		fi
 	fi
 	`'
 else
-	__prompt_ip_addr='`
-	[ ${PROMPT_IP_ADDR} -eq 1 ] && printf
-	`'
+       __prompt_ip_addr='`
+       [ ${PROMPT_IP_ADDR} -eq 1 ] && printf ":" && REPLACE
+       `'
 	__prompt_ip_addr_local_ip="`
 		if whereis nc &>/dev/null && hostname -I &>/dev/null
 		then
-			nc -w 3 -z 8.8.8.8 53 && hostname -I | cut -d' ' -f1 || echo OFFLINE
+			nc -w 3 -z 8.8.8.8 53 && hostname -I | cut -d' ' -f1 || echo 'OFFLINE'
 		else
-			ip addr | grep inet | grep global | head -n1 | tr ' ' '\t' | tr '//' '\t'| cut -f6 || echo OFFLINE
+			ip addr | grep inet | grep global | head -n1 | tr ' ' '\t' | tr '//' '\t'| cut -f6 || echo 'OFFLINE'
 		fi
 	`"
-	export __prompt_ip_addr=$(echo $__prompt_ip_addr | sed "s/printf/printf \"${__prompt_ip_addr_local_ip}\"/")
+	export __prompt_ip_addr=$(echo $__prompt_ip_addr | sed "s/REPLACE/printf \"${__prompt_ip_addr_local_ip}\"/")
 fi
 
--- prompt/wrk_dir.sh
+++ prompt/wrk_dir.sh
@@ -4,8 +4,8 @@
 # Always leave room for at least 20 chars of command
 # 55 is the stated length of the nonDir_part of the prompt
 # TERM_WIDTH - 55 - (length of pwd)
-__prompt_wrk_dir='`
-if [ $PROMPT_WRK_DIR -eq 1 ]
+export __prompt_wrk_dir='`
+if [ $PROMPT_WRK_DIR -eq 1 ] && printf ":"
 then
 	[ $(( $(tput cols) - 55 - ${#PWD} )) -lt 20 ] && printf $(printf ${PWD} | sed "s#.*/##") || printf ${PWD}
 fi
