5f0ef17e2e4241e3f4ac139766e91675c95b7ef2
Erez Binyamin <exb4096@rit.edu>
2020-07-23 00:01:39 -0400

    Change dirs to init dir in top.sh

--- top.sh
+++ top.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # This script gets called from a users bashrc
-
+cd ${INIT_DIR}
 source chubin.sh
 source git.sh
 source manpage_colors.sh
 source misc.sh
 source prompt.sh
+cd - > /dev/null
