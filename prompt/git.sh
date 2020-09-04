b3c576d6f8d62c2ec4067903d5602a537ae9392e
Erez Binyamin <exb4096@rit.edu>
2020-09-04 00:03:03 -0400

    Upgraded git status coloring/symbols

--- prompt/git.sh
+++ prompt/git.sh
@@ -74,9 +74,9 @@ then
 	else
 		printf " \[\033[1;38;5;2m\]"
 		git diff-index --quiet --cached HEAD -- 2>/dev/null || printf "\[\033[1;38;5;3m\]"
-		git diff --quiet 2>/dev/null || printf "\[\033[1;38;5;1m\]${GIT_EDIT_FILE_SYMBOL}"
-		git status -s | cut -d" " -f1 | grep -q -m 1 "??" && printf "\[\033[1;38;5;1m\]${GIT_NEW_FILE_SYMBOL}"
-		git status -s | cut -d" " -f2 | grep -q -m 1 "D" && printf "\[\033[1;38;5;1m\]${GIT_REM_FILE_SYMBOL}"
+		git status -s | cut -c1-2 | grep -q -m 1 " M" && printf "\[\033[1;38;5;1m\]${GIT_EDIT_FILE_SYMBOL}"
+		git status -s | cut -c1-2 | grep -q -m 1 "??" && printf "\[\033[1;38;5;1m\]${GIT_NEW_FILE_SYMBOL}"
+		git status -s | cut -c1-2 | grep -q -m 1 " D" && printf "\[\033[1;38;5;1m\]${GIT_REM_FILE_SYMBOL}"
 	fi
 fi
 `'
