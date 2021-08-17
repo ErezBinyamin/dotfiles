04308be25dbddfcf3c6737d5cd9db03e23f52137
Erez Binyamin <ezbn2532@gmail.com>
2021-08-17 18:25:06 -0400

    Arduino docker container auto-finds arduino devices!

--- docker/docker.sh
+++ docker/docker.sh
@@ -6,28 +6,37 @@ arduino() {
     # Launches arduino GUI
     # May need to install python dependencies
     # docker exec --user root -it <Container Name> bash -c "apt update; apt install python python-pip; pip install pyserial"
-    DEVICE=${1:-/dev/ttyUSB0}
     CHOME=/home/developer
+    DEVICE_ARGS=()
+    
+    echo "Discovering Arduino devices ...."
+    for D in $(ls -l /dev/serial/by-id/ | grep 'Arduino' | grep -o 'tty.*')
+    do
+        DEVICE_ARGS+=( "--device /dev/${D}:/dev/${D}" )
+	echo "Found: ${D}"
+    done
+    if [ ${#DEVICE_ARGS[@]} -eq 0 ]
+    then
+        echo "No Arduino devices found in /dev/serial/by-id/"
+        return 1
+    fi
     if [ ! -d $HOME/Arduino ]
     then
         echo "DirectoryNotFound: $HOME/Arduino"
         return 1
     fi
-    if [ ! -e ${DEVICE} ]
-    then
-        echo "FileNotFound: $DEVICE"
-        return 1
-    fi
+    set -x
     docker run \
         -it \
         --rm \
         --network=host \
         -e DISPLAY=$DISPLAY \
         -v $HOME/.Xauthority:${CHOME}/.Xauthority \
-        --device ${DEVICE}:${DEVICE} \
+        ${DEVICE_ARGS[@]} \
         -v $HOME/Arduino:${CHOME}/Arduino \
         tombenke/darduino \
         arduino
+    set +x
     return $?
 }
 
