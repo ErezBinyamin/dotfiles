0ec4faf62ec92afaecfca02d81efd2b13cd2f171
Erez Binyamin <ezbn2532@gmail.com>
2021-09-15 19:26:55 -0400

    docker.sh: added cAdvisor container function

--- docker/docker.sh
+++ docker/docker.sh
@@ -137,6 +137,32 @@ pihole() {
 	fi
 }
 
+cAdvisor() {
+	local PORT=$(next_port 8080)
+	if docker ps | grep -q cadvisor
+	then
+		echo "Bringing down cAdvisor container.."
+		sleep 1
+		docker container stop cadvisor
+		docker container prune
+	else
+		echo "cAdvisor serving on port: ${PORT}"
+		sleep 1
+		docker run \
+			--volume=/:/rootfs:ro \
+			--volume=/var/run:/var/run:ro \
+			--volume=/sys:/sys:ro \
+			--volume=/var/lib/docker/:/var/lib/docker:ro \
+			--volume=/dev/disk/:/dev/disk:ro \
+			--publish=${PORT}:8080 \
+			--detach=true \
+			--name=cadvisor \
+			--privileged \
+			--device=/dev/kmsg \
+			gcr.io/cadvisor/cadvisor:v0.40.0
+	fi
+}
+
 # Usefull docker aliases/functions
 docker-del() (
 	usage() {
