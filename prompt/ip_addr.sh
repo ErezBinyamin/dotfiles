f790818eed7279a39d8328b834aea960d4d2d0e9
Erez Binyamin <>
2025-01-18 15:39:07 -0500

    ip_addr.sh: +__prompt_ip_addr_func for cleaner variable definition in addition to adding function to env

--- prompt/ip_addr.sh
+++ prompt/ip_addr.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
 
-# IP address definition, either live or on source define
-if [ $PROMPT_SLOW_NETWORK -eq 0 ]
-then
-	export __prompt_ip_addr='`
-	if [ ${PROMPT_IP_ADDR} -eq 1 ] && printf ":"
-	then
-		if net_check
-		then
-			if hostname -I &>/dev/null
-			then
-				hostname -I | cut -d" " -f1
-			elif ip addr &>/dev/null
-			then
-				ip addr | grep inet | grep global | head -n1 | tr " " "\t" | tr "//" "\t"| cut -f6
-			elif ifconfig &>/dev/null
-				then
-					ifconfig | grep "inet " | grep -ve "inet.*\.1 .*n" -e "inet.*\.255 .*n" | grep -o "inet.*n" | cut -d" " -f2
-			fi
-		else
-			echo "OFFLINE"
-		fi
-	fi
-	`'
-else
-       __prompt_ip_addr='`
-       [ ${PROMPT_IP_ADDR} -eq 1 ] && printf ":" && REPLACE
-       `'
-	__prompt_ip_addr_local_ip="`
+__prompt_ip_addr_func() {
+    local ERRVAL
 		if [ ${PROMPT_IP_ADDR} -eq 1 ]
 		then
 			if net_check
@@ -42,11 +16,21 @@ else
 				then
 					ifconfig | grep 'inet ' | grep -ve 'inet.*\.1 .*n' -e 'inet.*\.255 .*n' | grep -o 'inet.*n' | cut -d' ' -f2
 				fi
+        ERRVAL=0
 			else
 				echo "OFFLINE"
+        ERRVAL=1
 			fi
 		fi
-	`"
+    return $ERRVAL
+}
+
+# IP address definition, either live or on source define
+if [ $PROMPT_SLOW_NETWORK -eq 0 ]
+then
+	export __prompt_ip_addr='`__prompt_ip_addr_func`'
+else
+  __prompt_ip_addr='`[ ${PROMPT_IP_ADDR} -eq 1 ] && printf ":" && REPLACE`'
+	__prompt_ip_addr_local_ip="`__prompt_ip_addr_func`"
 	export __prompt_ip_addr=$(echo $__prompt_ip_addr | sed "s/REPLACE/printf \"${__prompt_ip_addr_local_ip}\"/")
 fi
-
