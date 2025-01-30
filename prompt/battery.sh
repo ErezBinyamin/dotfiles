7c5b57a64b64544e08d71ea9d7ecb7db858a7156
Erez Binyamin <>
2025-01-29 22:54:40 -0500

    prompt/ using escape codes for coloring

--- prompt/battery.sh
+++ prompt/battery.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+export GRN='\[\033[38;5;10m\]'
+export YLW='\[\033[38;5;11m\]'
+export ORG='\[\033[38;5;202m\]'
+export RED='\[\033[38;5;9m\]'
+export BNK='\[\033[5m\]'
+
 # Battery life colored and charging status
 __prompt_battery_func() {
   local BATTERY_CHARGING="»"
+  unset __prompt_battery_str
   if [ ${PROMPT_BATTERY:-0} -eq 1 ] && [ -d /sys/class/power_supply/ ] && ls /sys/class/power_supply/ | grep -q BAT && [ $(echo $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) | wc -c) -gt 3 ]
   then
-  	[[ $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) != "Discharging" ]] && printf "${BATTERY_CHARGING}"
+  	[[ $(find /sys/class/power_supply/BAT*/ -name status -exec cat {} \;) != "Discharging" ]] && __prompt_battery_str="${BATTERY_CHARGING}"
   	local BAT=$(find /sys/class/power_supply/BAT*/ -name capacity -exec cat {} \;)
-  	[ $BAT -ge 75 -a $BAT -lt 101 ] && printf '\033[38;5;10m'"[||${BAT}||]" # Green  with 4 bars 
-  	[ $BAT -ge 50 -a $BAT -lt 75 ]  && printf '\033[38;5;11m'"[||${BAT}| ]" # Yellow with 3 bars
-  	[ $BAT -ge 25 -a $BAT -lt 50 ] && printf '\033[38;5;202m'"[||${BAT}  ]" # Orange with 2 bars
-  	[ $BAT -ge 10 -a $BAT -lt 25 ]   && printf '\033[38;5;9m'"[| ${BAT}  ]" # Red    with 1 bar
-  	[ $BAT -lt 10 ]           && printf '\033[38;5;9m\033[5m'"[| ${BAT}  ]" # Red    with 1 bar and blinking
+  	[ $BAT -ge 75 -a $BAT -lt 101 ] && __prompt_battery_str+="${GRN}[||${BAT}||]" # Green  with 4 bars 
+  	[ $BAT -ge 50 -a $BAT -lt 75 ]  && __prompt_battery_str+="${YLW}[||${BAT}| ]" # Yellow with 3 bars
+  	[ $BAT -ge 25 -a $BAT -lt 50 ]  && __prompt_battery_str+="${ORG}[||${BAT}  ]" # Orange with 2 bars
+  	[ $BAT -ge 10 -a $BAT -lt 25 ]  && __prompt_battery_str+="${RED}[| ${BAT}  ]" # Red    with 1 bar
+  	[ $BAT -lt 10 ]                 && __prompt_battery_str+="${RED}${BNK}[| ${BAT}  ]" # Red    with 1 bar and blinking
   fi
+  export __prompt_battery_str
 }
-export __prompt_battery='`__prompt_battery_func`'
--- prompt/prompt.sh
+++ prompt/prompt.sh
@@ -10,69 +10,77 @@ do
 done
 
 # CAPS LOCK notification symbol
-export __prompt_caps_lock='`
-PROMPT_CAPS_LOCK_SYMBOL="©"
-if [ ${PROMPT_CAPS_LOCK:-0} -eq 1 ]
-then
-	if [ ${PROMPT_CAPS_LOCK:-0} -eq 1 ] && xset -h &>/dev/null
-	then
-		xset q | grep -q "00: Caps Lock:   off" || printf "${PROMPT_CAPS_LOCK_SYMBOL}"
-	fi
-fi
-`'
+__prompt_capslock_func(){
+  PROMPT_CAPS_LOCK_SYMBOL="©"
+  __prompt_capslock_str=''
+  if [ ${PROMPT_CAPS_LOCK:-0} -eq 1 ] && xset -h &>/dev/null
+  then
+  	xset q | grep -q "00: Caps Lock:   off" || __prompt_capslock_str="${PROMPT_CAPS_LOCK_SYMBOL}"
+  fi
+  export __prompt_capslock_str
+}
 
 # Define ending symbol
 #	ssh    = §
 #	docker = {
 #	else   = $
-export __prompt_ending='`
-PROMPT_SSH_SYMBOL="§ "
-PROMPT_DOCKER_SYMBOL="{ "
-PROMPT_DEFAULT_SYMBOL="\$ "
-if [ ${PROMPT_ENV_ENDING:-0} -eq 1 ]
-then
-	if [ ! -x ${SSH_CLIENT+x} ]
-	then
-		printf "${PROMPT_SSH_SYMBOL}"
-	elif grep -q docker /proc/1/cgroup || [ -f /.dockerenv ]
-	then
-		printf "${PROMPT_DOCKER_SYMBOL}"
-	else
-		printf "${PROMPT_DEFAULT_SYMBOL}"
-	fi
-else
-	printf "${PROMPT_DEFAULT_SYMBOL}"
-fi
-`'
+__prompt_ending_func(){
+  PROMPT_SSH_SYMBOL="§ "
+  PROMPT_DOCKER_SYMBOL="{ "
+  PROMPT_DEFAULT_SYMBOL="\$ "
+  if [ ${PROMPT_ENV_ENDING:-0} -eq 1 ]
+  then
+  	if [ ! -x ${SSH_CLIENT+x} ]
+  	then
+  		printf "${PROMPT_SSH_SYMBOL}"
+  	elif grep -q docker /proc/1/cgroup || [ -f /.dockerenv ]
+  	then
+  		__prompt_ending_str="${PROMPT_DOCKER_SYMBOL}"
+  	else
+  		__prompt_ending_str="${PROMPT_DEFAULT_SYMBOL}"
+  	fi
+  else
+  	__prompt_ending_str="${PROMPT_DEFAULT_SYMBOL}"
+  fi
+  export __prompt_ending_str
+}
+__prompt_ending_func
 
+__prompt_command(){
+  __prompt_battery_func
+  __prompt_capslock_func
+  #__prompt_ending_func
+}
+PROMPT_COMMAND=__prompt_command
+export NC='\[\033[0m\]\[\033[39;49m\]'
 #################################################
-#			                        #
-#	----ACTUALLY SET THE PS1----		#
-#			                        #
+#	                                              #
+#	         ----ACTUALLY SET THE PS1----         #
+#                                               #
 #################################################
 if [ ${PROMPT_DEFAULT:-0} -ne 1 ]
 then
 	unset PS1
-	PS1="${__prompt_battery}"				# Battery life
-	PS1+='\033[0m'
+	PS1="\${__prompt_battery_str@P}"				# Battery life
+	PS1+="${NC}"
 	PS1+="${__prompt_COLOR_1}${__prompt_datetime}"		# Date and time
-	PS1+='\033[0m'
+	PS1+="${NC}"
 	PS1+="${__prompt_COLOR_2}${USER:-SUPERUSER}"		# Username
-	PS1+='\033[0m'
+	PS1+="${NC}"
 	PS1+="${__prompt_COLOR_3}${__prompt_ipaddr}"		# IP address
-	PS1+='\033[0m'
+	PS1+="${NC}"
 	PS1+="${__prompt_COLOR_4}${__prompt_wrkdir}"		# Working directory
-	PS1+='\033[0m'
+	PS1+="${NC}"
 	PS1+="${__prompt_git_repo}"				# Repo name
-	PS1+='\033[0m'
+	PS1+="${NC}"
 	PS1+="${__prompt_git_pull}${__prompt_git_push}"		# Push pull arrows
-	PS1+='\033[0m'
+	PS1+="${NC}"
 	PS1+="${__prompt_git_color}${__prompt_git_branch}"	# Colored git branch/status
-	PS1+='\033[0m'
-	PS1+="${__prompt_caps_lock}"				# Caps lock notification
-	PS1+='\033[0m'
-	PS1+="${__prompt_ending}"				# End with: "${__prompt_ending}"
-	PS1+='\033[0m'
+	PS1+="${NC}"
+	PS1+="\${__prompt_capslock_str@P}"				# Caps lock notification
+	PS1+="${NC}"
+	PS1+="\${__prompt_ending_str@P}"				# End with: "${__prompt_ending}"
+	PS1+="${NC}"
 
 	unset PS1_noDir
 	PS1_noDir="${__prompt_battery}"			 # Battery life
