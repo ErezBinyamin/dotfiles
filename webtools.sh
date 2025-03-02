bdbf3c1ac4169809a99851f42f91fc61338eac06
Erez Binyamin <>
2025-03-02 15:14:57 -0500

    removed: wiki and define, moved curly to webtools

--- webtools.sh
+++ webtools.sh
@@ -4,6 +4,7 @@ alias weather='curl wttr.in'
 alias moon='curl wttr.in/Moon'
 alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -'
 alias map='printf "\n\nA:\tZoom In +\nZ:\tZoom Out -\nArrows:\tMove\nQ:\tQuit\n\nPress any key to continue...\n"; read -s -n 1; telnet mapscii.me'
+alias curly='curl -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" --compressed --retry 5 --retry-delay 3 --connect-timeout 10 --max-time 30 --fail --insecure -b cookies.txt -c cookies.txt -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Accept-Language: en-US,en;q=0.9" -H "Cache-Control: max-age=0" -H "Connection: keep-alive" -H "Upgrade-Insecure-Requests: 1"'
 
 # Help from cheat.sh git repo:
 cheat() {
@@ -63,40 +64,5 @@ share_get() {
 		return 1
 	fi
 	return 0
-}
-
-# Command line dictionary
-define() {
-	if net_check
-	then
-		curl --silent https://www.vocabulary.com/dictionary/$(echo $@ | tr ' ' '-') | \
-		grep 'og:description' | sed 's/&#[0-9][0-9][0-9]//g' | awk -F "\"" '{print $4}'
-	else
-		>&2 echo "ERROR: No network connectivity"
-		return 1
-	fi
-	return 0
-}
-
-# Command line wikepedia
-wiki() {
-    if ! net_check
-    then
-	    >&2 echo "ERROR: No network connectivity"
-            return 1
-    fi
-    QUERY=$(echo $@ | tr ' ' '+' | tr -d "'" | tr -d '"')
-
-    GOOG="https://www.google.com/search?q=wikepedia+${QUERY}"
-    QUERY=$(curl --silent --user-agent 'Mozilla/5.0 (MSIE; Windows 10)' ${GOOG} \
-            | grep -Po 'https://en.wikipedia[.]org.*?"' \
-            | head -n1 \
-            | tr -d '"' \
-            | sed 's|https://||' \
-            | xargs basename)
-
-    WIKI="https://en.wikipedia.org/w/api.php?action=query&format=json&titles=${QUERY}&prop=extracts&exintro&explaintext"
-    curl --silent --user-agent 'Mozilla/5.0 (MSIE; Windows 10)' ${WIKI}
-    printf '\n\n'
 }
 
